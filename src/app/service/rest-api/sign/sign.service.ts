e37a9f5c9a81fb672d2d47c1d9c54a777887454e
yeon <yeoncheol.jang@gmail.com>
2021-07-19 12:45:40 +0900

    [Changed] KakaoTokenController API URI

--- src/app/service/rest-api/sign/sign.service.ts
+++ src/app/service/rest-api/sign/sign.service.ts
@@ -11,7 +11,7 @@ export class SignService {
 
   private readonly signInUrl = '/api/v1/signIn';
   private readonly signUpUrl = '/api/v1/signUp';
-  private readonly getKakaoAuthCodeUrl = '/social/login/kakaoAuthCode';
+  private readonly getKakaoAuthCodeUrl = '/api/v1/get/kakaoAuthCode';
   private readonly signUpOrInKakaoAuthcode = '/api/v1/signUpOrIn/kakaoAuthCode';
 
   constructor(private http: HttpClient,
